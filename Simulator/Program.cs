6c2a57d86cc04be5d534a42c91c31fb0463d03cd
Moe <moritzhauer@freenet.de>
2017-06-05 23:17:50 +0200

    Add SimClient

---
+++ Simulator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+using ecruise.Models;
+using ecruise;
+
+namespace Simulator
+{
+    class Program
+    {
+       
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            Console.WriteLine("Simulator wurde gestartet");
+            // New code:
+            try
+            {
+                SimClient client = new SimClient();
+                Response r = await client.LoginAsync("admin@ecruise.me", "ecruiseAdmin123!!!");
+                Console.WriteLine(r.Token.ToString());
+                client.access_token = r.Token;
+
+                Car car = await client.Cars2Async(1, "");
+                Console.WriteLine(car.ToString());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
