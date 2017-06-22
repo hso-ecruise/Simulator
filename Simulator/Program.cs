442657e0db967add6d684dd274a554cf6b351077
Moe <moritzhauer@freenet.de>
2017-06-22 17:19:51 +0200

    Remove TaskScheduler

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -10,35 +10,15 @@ namespace Simulator
 {
     class Program
     {
-       
+
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
-           // RunAsync().Wait();
 
             JobManager.Initialize(new BackgroundTasker().ScheduleBookings());
-            
-            Console.ReadLine();
-        }
-
-        static async Task RunAsync()
-        {
-            Console.WriteLine("Simulator wurde gestartet");
-            // New code:
-            try
-            {
-                await TaskScheduler.loginAsAdmin();
-
-                await TaskScheduler.getCarbyId(1,5000);
-                
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
 
             Console.ReadLine();
         }
